2f0aca20a68b21bb7634bba4b0fe852af48a9897
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-06-10 09:00:14 +0000

    マイグレーションファイル記述

--- db/migrate/20230610045941_create_games.rb
+++ db/migrate/20230610045941_create_games.rb
@@ -1,6 +1,9 @@
 class CreateGames < ActiveRecord::Migration[6.1]
   def change
     create_table :games do |t|
+      t.references :genre, foreign_key: true,  null: false
+      t.text :name, null: false
+      t.text :explanation, null: false
 
       t.timestamps
     end

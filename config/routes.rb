d5848d87bd1381b4ee47c7ee3d85b47a0cb8d9b5
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-06-10 10:54:31 +0000

    コントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,15 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+
+  namespace :admin do
+    root :to => "homes#top"
+    resources :users, only: [:index,:show,:edit,:update]
+    resources :genres, only: [:index,:create,:edit,:update]
+    resources :games, only: [:index,:new,:create,:show,:edit,:update,:destroy]
+    resources :reviews, only: [:show,:destroy]
+  end
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

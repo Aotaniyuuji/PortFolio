27b6e1951036b69a03712ac47a151a2edcc28e4f
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-06-10 09:55:05 +0000

    jqueryのインストール

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
+// 星5レビュー機能用
+const webpack = require('webpack')
 
+environment.plugins.prepend('Provide',
+  new webpack,ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    jquery: 'jquery/src/jquery',
+  })
+  )
 module.exports = environment
--- db/migrate/20230610080434_create_reviews.rb
+++ db/migrate/20230610080434_create_reviews.rb
@@ -1,6 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
+      t.references :user, foreign_key: true, null: false
+      t.references :game, foreign_key: true, null: false
+      t.integer :rating, null: false
+      t.text :comment
 
       t.timestamps
     end

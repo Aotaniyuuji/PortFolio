049c4db3e86b0650e8ea296ccbf7194153a3e874
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-06-11 02:06:18 +0000

    bootstrap導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,7 +2,8 @@ const { environment } = require('@rails/webpacker')
 // 星5レビュー機能用
 const webpack = require('webpack')
 
-environment.plugins.prepend('Provide',
+environment.plugins.prepend(
+  'Provide',
   new webpack,ProvidePlugin({
     $: 'jquery/src/jquery',
     jQuery: 'jquery/src/jquery',

2a1d313e0cc6037c5d967ebae85eb86e3edf0659
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-06-12 07:11:42 +0000

    ハンバーガーアイコン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(resource_or_scope)
-    unless resource_or_scope == :admin
+    if resource_or_scope == :user
       games_path
     else
       admin_games_path

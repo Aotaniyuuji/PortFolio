671302dea1ab72e5eab988a3e57182381f51e1dc
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-06-13 11:55:23 +0000

    検索機能の実装

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,6 +2,6 @@ class Public::SearchesController < ApplicationController
   before_action :authenticate_user!
 
   def search
-    @game = Game.looks(params[:search], params[:word])
+    @games = Game.looks(params[:search], params[:word])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,6 @@ Rails.application.routes.draw do
     end
   end
 
-  get "search" => "searches#search"
-  get "result" => "searches#result"
+  get "search" => "public/searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

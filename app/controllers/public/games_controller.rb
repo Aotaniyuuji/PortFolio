81b4b9198e92ba0f5ee4236c822194bb55d4e995
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-06-14 08:32:06 +0000

    ジャンル別表示制作中

--- app/controllers/public/games_controller.rb
+++ app/controllers/public/games_controller.rb
@@ -1,10 +1,16 @@
 class Public::GamesController < ApplicationController
   def index
+    @genres = Genre.all
     @games = Game.all
   end
 
   def show
     @game = Game.find(params[:id])
     @review = Review.new
+  end
+
+  def bygenre_game
+    @genres = Genre.all
+    @game = Genre.where(name: "RPG")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -34,5 +34,6 @@ Rails.application.routes.draw do
   end
 
   get "search" => "public/searches#search"
+  get "bygenre_game" => "public/games#bygenre_game"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

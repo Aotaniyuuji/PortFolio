bad458ca7bfcd8061523fe96cbcdd7bfeec1d20a
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-06-14 03:03:02 +0000

    ジャンル編集作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -11,12 +11,16 @@ class Admin::GenresController < ApplicationController
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
-  end
-
-  def destroy
+    genre = Genre.find(params[:id])
+    if genre.update(genre_params)
+      redirect_to admin_genres_path
+    else
+      redirect_to request.referer
+    end
   end
 
   private

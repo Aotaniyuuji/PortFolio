94abc6b4ea263d72872580bf722cb2c4c67affb6
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-06-16 03:53:15 +0000

    バリデーション設定

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :game
+
+  validates :title, presence: true, length: {maximum: 20}
+  validates :comment, presence: true, length: {maximum: 200}
+  validates :star, presence: true
 end

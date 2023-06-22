ecd3046408a68f4692df0a6a20b308f2701e3a4b
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-06-22 12:59:08 +0000

    [update]ページネーション実装

--- app/controllers/public/games_controller.rb
+++ app/controllers/public/games_controller.rb
@@ -3,20 +3,20 @@ class Public::GamesController < ApplicationController
     @genres = Genre.all
 # 絞り込み条件によって条件分岐
     if params[:genre_id]
-      @games = Game.where(genre_id: params[:genre_id])
+      @games = Game.where(genre_id: params[:genre_id]).page(params[:page]).per(5)
     else
-      @games = Game.all
+      @games = Game.page(params[:page]).per(5)
     end
 
 # ソート機能による並び替え
     if params[:latest]
-      @games = Game.latest
+      @games = Game.latest.page(params[:page]).per(5)
     elsif params[:old]
-      @games = Game.old
+      @games = Game.old.page(params[:page]).per(5)
     elsif params[:star_count]
-      @games = Game.star_count
+      @games = Game.star_count.page(params[:page]).per(5)
     else
-      @games = Game.all
+      @games = Game.page(params[:page]).per(5)
     end
   end
 

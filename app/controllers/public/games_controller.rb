62a84eea358aac8c06a79bf7543c7cfc7de095ec
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-06-23 01:44:17 +0000

    [update]星評価表示と絞り込み条件とソートの共存

--- app/controllers/public/games_controller.rb
+++ app/controllers/public/games_controller.rb
@@ -1,15 +1,12 @@
 class Public::GamesController < ApplicationController
+
   def index
     @genres = Genre.all
 # 絞り込み条件によって条件分岐
     if params[:genre_id]
       @games = Game.where(genre_id: params[:genre_id]).page(params[:page]).per(5)
-    else
-      @games = Game.page(params[:page]).per(5)
-    end
-
 # ソート機能による並び替え
-    if params[:latest]
+    elsif params[:latest]
       @games = Game.latest.page(params[:page]).per(5)
     elsif params[:old]
       @games = Game.old.page(params[:page]).per(5)

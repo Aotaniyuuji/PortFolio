277554b7cc79be358c302391ecaf2cc586bdb04e
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-06-23 11:00:06 +0000

    [update]admin側ユーザー一覧画面修正とユーザーshowページ作成

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -1,2 +1,8 @@
 class Admin::ReviewsController < ApplicationController
+
+  def destroy
+    Review.find(params[:id]).destroy
+    flash[:success] = "レビューを削除しました。"
+    redirect_to admin_game_path(params[:game_id])
+  end
 end

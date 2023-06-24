f166bc05276bb0430ebac4cd69e6ec9bf14cdda6
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-06-24 03:44:33 +0000

    [update]ユーザー管理機能の追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,4 +6,20 @@ class Admin::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      flash[:success] = "ステータスを更新しました"
+      redirect_to admin_user_path(user.id)
+    else
+      redirect_to request.referer
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:is_deleted)
+  end
 end

7f6c182d36a21b238f4a572d716f33363e99b62d
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-06-26 14:28:59 +0000

    [update]seed.rb変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,42 +14,45 @@ Admin.create!(
 puts "________ create User"
 User.create!(
    [
-      {
-         name: 'test',
-         email: 'test@gmail.com',
-         password: '0000000'
-      }
+      {name: '田中太郎', email: 'tanaka@example.com', password: '0000000'},
+      {name: '山田花子', email: 'yamada@example.com', password: '1111111'},
+      {name: '佐々木小次郎', email: 'sasaki@example.com', password: '2222222'},
+      {name: '徳川家光', email: 'tokugawa@exmaple.com', password: '3333333'},
+      {name: '伊達政宗', email: 'date@example.com', password: '4444444'}
    ]
 )
 
 puts "________ create Genre"
 Genre.create!(
    [
-      {
-         name: "アクションRPG",
-         is_active: true
-      },
-      {
-         name: "ホラー",
-         is_active: true
-      },
-      {
-         name: "ソシャゲ",
-         is_active: true
-      },
-      {
-         name: 'アクション',
-         is_active: true
-      }
+      {name: "アクションRPG", is_active: true},
+      {name: "ホラー", is_active: true},
+      {name: "ソシャゲ", is_active: true},
+      {name: 'AVG', is_active: true}
    ]
 )
 
 puts "________ create Game"
 game = Game.create!(
-   name: 'モンスターハンターワールド：Ice Borne',
+   name: 'SEKIRO SHADOWS DIE TWICE',
    genre_id: 1,
-   explanation: '過去一やり込んだシリーズ！
-   新規のモンスターはもちろん、ハンターのアクションも多彩！'
+   explanation: 'フロムソフトウェアの代表作！
+   革新的なバトルシステムが楽しすぎる！日本ゲーム大賞2019優秀賞受賞！'
+)
+game.image.attach(io: File.open(Rails.root.join('app/asset/images/noimage.jpeg')),filename: 'noimage.jpeg')
+
+game = Game.create!(
+   name: 'バイオハザードRE:4',
+   genre_id: 2,
+   explanation: 'バイオ４のリメイク作品！ 期待を超える個人的神ゲー！！'
+)
+game.image.attach(io: File.open(Rails.root.join('app/asset/images/noimage.jpeg')),filename: 'noimage.jpeg')
+
+game = Game.create!(
+   name: 'ELDEN RING',
+   genre_id: 1,
+   explanation: 'フロムソフトウェアの死にゲー最新作！
+   世界観良し！アクション性良し！バトル良し！操作性！日本ゲーム大賞2022受賞!'
 )
 game.image.attach(io: File.open(Rails.root.join('app/asset/images/noimage.jpeg')),filename: 'noimage.jpeg')
 
@@ -57,14 +60,28 @@ game = Game.create!(
    name: 'モンスターハンターワールド：Ice Borne',
    genre_id: 1,
    explanation: '過去一やり込んだシリーズ！
-   新規のモンスターはもちろん、ハンターのアクションも多彩！'
+   新規のモンスターが多く、ハンターのアクションも多彩！'
 )
 game.image.attach(io: File.open(Rails.root.join('app/asset/images/noimage.jpeg')),filename: 'noimage.jpeg')
+
 game = Game.create!(
-   name: 'モンスターハンターワールド：Ice Borne',
-   genre_id: 1,
-   explanation: '過去一やり込んだシリーズ！
-   新規のモンスターはもちろん、ハンターのアクションも多彩！'
+   name: '原神',
+   genre_id: 3,
+   explanation: 'オープンワールドRPG！基本無料なので気軽に遊べる。後キャラデザインがいい'
+)
+game.image.attach(io: File.open(Rails.root.join('app/asset/images/noimage.jpeg')),filename: 'noimage.jpeg')
+
+game = Game.create!(
+   name: 'あつまれ　どうぶつの森',
+   genre_id: 4,
+   explanation: 'どう森最新作！自分だけの島づくりができるアドベンチャーゲーム'
+)
+game.image.attach(io: File.open(Rails.root.join('app/asset/images/noimage.jpeg')),filename: 'noimage.jpeg')
+
+game = Game.create!(
+   name: 'バイオハザードRE:4',
+   genre_id: 2,
+   explanation: 'バイオ４のリメイク作品！ 期待を超える個人的神ゲー！！'
 )
 game.image.attach(io: File.open(Rails.root.join('app/asset/images/noimage.jpeg')),filename: 'noimage.jpeg')
 
